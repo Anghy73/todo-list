61455b7a92094da95f66a92f50318ea080dfded8
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2023-08-29 22:03:49 -0400

    localstorage add

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
-const list = document.querySelector('.list')
+const list = document.querySelector(".list");
 const template = document.querySelector("#template").content;
-const fragment = document.createDocumentFragment()
-
+const fragment = document.createDocumentFragment();
 
 let tasks = {};
 
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  paintTasks()
+});
 
-document.addEventListener('DOMContentLoaded', paintTasks)
-
-list.addEventListener('click', (e) => {btnAction(e)})
+list.addEventListener("click", (e) => {
+  btnAction(e);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -32,65 +37,74 @@ function addTask(e) {
 
   tasks[task.id] = task;
 
-  paintTasks()
+  paintTasks();
 
-  form.reset()
-  e.target.querySelector('input').focus()
+  form.reset();
+  e.target.querySelector("input").focus();
 }
 
 function paintTasks() {
 
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+  console.log(localStorage.getItem('tasks'));
+
+
   if (Object.values(tasks).length === 0) {
     list.innerHTML = `
       <p>Without Tasks 🔥</p>
-    `
-    return
+    `;
+    return;
   }
 
-  list.innerHTML = ''
+  list.innerHTML = "";
 
   Object.values(tasks).forEach((task) => {
-    const clone = template.cloneNode(true)
-    clone.querySelector('p').textContent = task.text
+    const clone = template.cloneNode(true);
+    clone.querySelector("p").textContent = task.text;
 
     if (task.status) {
-      clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-check', 'fa-rotate-left')
-      clone.querySelector('p').style.textDecoration = 'line-through'
+      clone
+        .querySelectorAll(".fa-solid")[0]
+        .classList.replace("fa-check", "fa-rotate-left");
+      clone.querySelector("p").style.textDecoration = "line-through";
     }
 
+    clone.querySelectorAll(".fa-solid")[0].dataset.id = task.id;
+    clone.querySelectorAll(".fa-solid")[1].dataset.id = task.id;
+    clone.querySelectorAll(".fa-solid")[2].dataset.id = task.id;
 
-    clone.querySelectorAll('.fa-solid')[0].dataset.id = task.id
-    clone.querySelectorAll('.fa-solid')[1].dataset.id = task.id
+    fragment.appendChild(clone);
+  });
 
-    fragment.appendChild(clone)
-  })
-
-  list.appendChild(fragment)
+  list.appendChild(fragment);
 }
-
 
 function btnAction(e) {
-  if (e.target.classList.contains('fa-check')) {
-    tasks[e.target.dataset.id].status = true
-    paintTasks()
+  if (e.target.classList.contains("fa-check")) {
+    tasks[e.target.dataset.id].status = true;
+    paintTasks();
   }
 
-  if (e.target.classList.contains('fa-x')) {
-    delete tasks[e.target.dataset.id]
-    paintTasks()
+  if (e.target.classList.contains("fa-x")) {
+    delete tasks[e.target.dataset.id];
+    paintTasks();
   }
 
-  if (e.target.classList.contains('fa-rotate-left')) {
-    tasks[e.target.dataset.id].status = false
-    paintTasks()
+  if (e.target.classList.contains("fa-rotate-left")) {
+    tasks[e.target.dataset.id].status = false;
+    paintTasks();
   }
 
-  if (e.target.classList.contains('fa-pen')) {
-    input.value = tasks[e.target.dataset.id].text
-    input.focus()
-    delete tasks[e.target.dataset.id]
-    paintTasks()
+  if (e.target.classList.contains("fa-pen")) {
+    input.value = tasks[e.target.dataset.id].text;
+    input.focus();
+    delete tasks[e.target.dataset.id];
+    paintTasks();
   }
 
-  e.stopPropagation()
+  e.stopPropagation();
 }
+
+
+console.log(localStorage.getItem('tasks'));
